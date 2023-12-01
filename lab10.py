090752264199424fbf230d37bd6f46b18022c225
Dallin Robison <dallinrobison@gmail.com>
2023-11-30 21:48:54 -0700

    added scatterplot

--- lab10.py
+++ lab10.py
@@ -4,8 +4,11 @@ import numpy as np
 import streamlit as st
 import seaborn as sns
 
-st.title('Penguins Interactive')
+st.title('Penguin Bill Length and Depth by Species and Island')
 penguins = sns.load_dataset('penguins')
 
 species = st.selectbox("Select a Species", penguins['species'].unique())
 island = st.selectbox("Select an Island", penguins[penguins['species'] == species]['island'].unique())
+graph_penguins = penguins[(penguins['species'] == species) & (penguins['island'] == island)]
+
+sns.scatterplot(graph_penguins, x = 'bill_depth_mm', y = 'bill_length_mm')
